64a87330838099e4d250e55f1befba2b5e7c70ec
Bui Duc Trung <buiductrung.cva@gmail.com>
2025-10-26 10:02:36 +0700

    Lesson 1

---
+++ Lesson1/script.js
@@ -0,0 +1,12 @@
+// Đây là nơi viết javascript
+
+// Hiển thị ra màn hình
+console.log("Hello World!");
+
+// Biến số - Variable
+// Biến số là nơi để lưa trữ dữ liệu, có thể thay đổi được
+let name = "Duc Trung";
+var age = 2;
+console.log("Họ tên: ", name);
+console.log("Tuổi: ", age);
+console.log(`Họ tên: ${name}, Tuổi: ${age}`);
