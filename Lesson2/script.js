ed7dd6ced1fb5cd191a5f97b08f02926072009d0
Bui Duc Trung <buiductrung.cva@gmail.com>
2025-11-02 08:40:02 +0700

    Lesson 2

---
+++ Lesson2/script.js
@@ -0,0 +1,33 @@
+// Hiển thị ra console
+console.log("Hello, MindX!");
+
+// Thông báo (popup)
+// alert("đây là thông báo")
+
+// Variables - Biến số
+    // Dùng để lưu trữ dữ liệu
+    // Có thể thể thay đổi giá trị
+
+    // var: global (dùng được ở mọi nơi trong file)
+var name = "Duc Trung";
+name = "Hải meme"
+    // let: local (chỉ dùng được trong khối lệnh chứa nó)
+let age = 2;    
+
+// Constants - Hằng số
+    // Dùng để lưu trữ dữ liệu
+    // Không thể thay đổi giá trị
+const PI = 3.14;
+
+// Data Types - Kiểu dữ liệu
+    // Primitive Types - Kiểu dữ liệu nguyên thủy
+        // String: chuỗi ký tự / xâu ký tự
+let adress = "Hà Nội";
+        // Number: Số (cả số nguyên và số thực), có cả Infinity và NaN (not a number)
+let height = 1.75;
+        // Boolean: đúng (true) hoặc sai (false)
+let isStudent = false;
+        // Null: giá trị rỗng, thường dùng để xóa/reset giá trị
+let emptyValue = null;
+        // Undefined: giá trị chưa được định nghĩa
+let notDefined;
