0b9bd5434a80d1e08d468159fb69e2b064eb2e7c
Bui Duc Trung <buiductrung.cva@gmail.com>
2025-11-09 09:55:25 +0700

    Lesson 3

--- Lesson3/script.js
+++ Lesson3/script.js
@@ -45,3 +45,33 @@ newDiv.setAttribute("class", "container");
 document.body.appendChild(newDiv);
 
 // ============ THỰC HÀNH ============
+
+    // btn1: Thay đổi nội dung tiêu đề h1
+function changeText() {
+    let h1 = document.getElementById("myH1");
+    h1.textContent = "Tiêu đề đã được thay đổi!";
+}
+
+    // btn2: Hiển thị cảnh báo
+function showAlert() {
+    alert("Bạn đã nhấn nút!");
+}
+
+    // btn3: Thay đổi màu nền 
+const btn3 = document.getElementById("btn3");
+btn3.addEventListener("click", function() {
+    document.body.style.backgroundColor = "lightblue";
+});
+
+    // Kiểm tra tuổi
+document.getElementById("mySubmitAge").onclick = function() {
+    let age = parseInt(document.getElementById("myAge").value);
+    if (isNaN(age)) { // Kiểm tra nếu không phải số - Not a Number
+        alert("Vui lòng nhập một số hợp lệ cho tuổi.");
+    } else if (age >= 18) {
+        alert("Bạn đã đủ 18 tuổi.");
+    } else {
+        alert("Bạn chưa đủ 18 tuổi.");
+    }
+    document.getElementById("myAge").value = "";
+}
