79e293021e222627ec613121835015f11281877d
Bui Duc Trung <buiductrung.cva@gmail.com>
2025-11-09 09:23:22 +0700

    Lesson 3

--- Lesson3/script.js
+++ Lesson3/script.js
@@ -10,3 +10,26 @@ console.log(p1);
     // Lấy phần tử theo class name
 let myClassElements = document.getElementsByClassName("myClass");
 console.log(myClassElements);
+
+    // Lấy phần tử theo tag name
+let pTags = document.getElementsByTagName("p");
+console.log(pTags);
+
+    // querrySelectorAll: lấy tất cả phần tử theo selcetor
+let allParagraphs = document.querySelectorAll(".myClass");
+console.log(allParagraphs);
+
+    // querrySelector: lấy phần tử đầu tiên theo selector
+let firstParagraph = document.querySelector(".myClass");
+console.log(firstParagraph);
+
+    // innerHTML: lấy hoặc gán nội dung HTML (bao gồm cả tên thẻ)
+document.getElementById("myH1").innerHTML = "<h1>JSA29</h1>";
+myP.innerHTML = "<p>Hải meme ăn sáng trong lớp</p>";
+
+    // textContent: lấy hoặc gán content (không bao gồm thẻ)
+myP.textContent = "Hải meme ăn sáng trong lớp - textContent";
+
+    // style: thay đổi CSS của phần tử
+header.style.color = "darkred";
+header.style.fontFamily = "Momo Signature";
