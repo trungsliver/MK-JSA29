1d2e044d1741a386d454b3c24904949882c4d7fa
Bui Duc Trung <buiductrung.cva@gmail.com>
2025-11-09 08:37:55 +0700

    Lesson 3

---
+++ Lesson3/script.js
@@ -0,0 +1,12 @@
+// DOM - Document Object Model
+    //  Cấu trúc mô phỏng HTML dưới dạng cây (tree) và các phần tử - nút (node)
+
+    // Lây phần tử theo id
+let header = document.getElementById("myH1");
+console.log(header);
+let p1 = document.getElementById("myP");
+console.log(p1);
+
+    // Lấy phần tử theo class name
+let myClassElements = document.getElementsByClassName("myClass");
+console.log(myClassElements);
